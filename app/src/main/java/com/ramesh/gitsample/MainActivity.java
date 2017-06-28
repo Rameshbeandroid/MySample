851f0c8d8094bcebcff2669b96dd8290ca6fa992
Ramesh <rameshbeandroid@gmail.com>
2017-06-28 11:17:43 +0530

    add toast function

--- app/src/main/java/com/ramesh/gitsample/MainActivity.java
+++ app/src/main/java/com/ramesh/gitsample/MainActivity.java
@@ -2,6 +2,7 @@ package com.ramesh.gitsample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayToast();
+    }
+
+    private void displayToast() {
+        Toast.makeText(this, "My sample", Toast.LENGTH_SHORT).show();
     }
 }
