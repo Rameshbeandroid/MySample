f90484f4568dbdd51dfc8524806e316c2d1ec666
lenovo\lencare <rameshbeandroid@gmail.com>
2017-11-05 22:24:43 +0530

    interface removed

--- app/src/main/java/com/ramesh/gitsample/login/LoginActivity.java
+++ app/src/main/java/com/ramesh/gitsample/login/LoginActivity.java
@@ -2,71 +2,30 @@ package com.ramesh.gitsample.login;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-
 import com.ramesh.gitsample.R;
 
-public class LoginActivity extends AppCompatActivity implements LoginView, View.OnClickListener {
+public class LoginActivity extends AppCompatActivity {
 
-    private ProgressBar progressBar;
-    private EditText username;
-    private EditText password;
-    private LoginPresenter presenter;
+  private ProgressBar progressBar;
+  private EditText username;
+  private EditText password;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
 
-        progressBar = (ProgressBar) findViewById(R.id.progress);
-        username = (EditText) findViewById(R.id.username);
-        password = (EditText) findViewById(R.id.password);
-        findViewById(R.id.button).setOnClickListener(this);
+    progressBar = (ProgressBar) findViewById(R.id.progress);
+    username = (EditText) findViewById(R.id.username);
+    password = (EditText) findViewById(R.id.password);
+  }
 
-        presenter = new LoginPresenterImpl(this);
-    }
+  @Override protected void onResume() {
+    super.onResume();
+  }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onDestroy() {
-        presenter.onDestroy();
-        super.onDestroy();
-    }
-
-    @Override
-    public void showProgress() {
-        progressBar.setVisibility(View.VISIBLE);
-    }
-
-    @Override
-    public void hideProgress() {
-        progressBar.setVisibility(View.GONE);
-    }
-
-    @Override
-    public void setUsernameError() {
-        username.setError(getString(R.string.username_error));
-    }
-
-    @Override
-    public void setPasswordError() {
-        password.setError(getString(R.string.password_error));
-    }
-
-    @Override
-    public void navigateToHome() {
-//        startActivity(new Intent(this, MainActivity.class));
-        finish();
-    }
-
-    @Override
-    public void onClick(View v) {
-        presenter.validateCredentials(username.getText().toString(), password.getText().toString());
-    }
+  @Override protected void onDestroy() {
+    super.onDestroy();
+  }
 }
