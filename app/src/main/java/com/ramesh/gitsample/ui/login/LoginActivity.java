cfee444f791ae8b11212d07a5d4f17e42dc5462a
lenovo\lencare <rameshbeandroid@gmail.com>
2018-01-19 12:27:25 +0530

    Merge branch 'Branch01' of E:\Learning\MySample with conflicts.

---
+++ app/src/main/java/com/ramesh/gitsample/ui/login/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.ramesh.gitsample.ui.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import com.ramesh.gitsample.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+  }
+
+
+
+  @Override protected void onResume() {
+    super.onResume();
+  }
+
+}
