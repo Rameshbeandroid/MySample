c02909b0543973be316a5f1a7e31b05eba4eab1e
lenovo\lencare <rameshbeandroid@gmail.com>
2018-01-19 12:46:31 +0530

    Branch02 toast added

--- app/src/main/java/com/ramesh/gitsample/ui/login/LoginActivity.java
+++ app/src/main/java/com/ramesh/gitsample/ui/login/LoginActivity.java
@@ -2,7 +2,7 @@ package com.ramesh.gitsample.ui.login;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.widget.Toast;
 import com.ramesh.gitsample.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -10,12 +10,14 @@ public class LoginActivity extends AppCompatActivity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login);
+    displayLogForBranch01();
   }
 
-
+  private void displayLogForBranch01() {
+    Toast.makeText(this, "Branch01", Toast.LENGTH_SHORT).show();
+  }
 
   @Override protected void onResume() {
     super.onResume();
   }
-
 }
