7c55b4f0ac1c61c6cde7def09839ec702adf3196
lenovo\lencare <rameshbeandroid@gmail.com>
2018-01-19 11:56:08 +0530

    Layout view removed

--- app/src/main/java/com/ramesh/gitsample/ui/login/LoginActivity.java
+++ app/src/main/java/com/ramesh/gitsample/ui/login/LoginActivity.java
@@ -2,40 +2,23 @@ package com.ramesh.gitsample.ui.login;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
-import android.widget.EditText;
+import android.util.Log;
 import com.ramesh.gitsample.R;
-import com.squareup.sqlbrite3.BriteDatabase;
-import dagger.android.AndroidInjection;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import javax.inject.Inject;
 
-public class LoginActivity extends AppCompatActivity  {
-
-  private EditText etuserName;
-  private EditText etPassWord;
-  private Button btSingIn;
-
+public class LoginActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login);
-    intiUIElement();
-
-  }
-  private void intiUIElement() {
-    etuserName = (EditText) findViewById(R.id.et_user_name);
-    etPassWord = (EditText) findViewById(R.id.et_user_pass_word);
-    btSingIn = (Button) findViewById(R.id.bt_sign_in);
+    displayBranchOne();
   }
 
-
+  private void displayBranchOne() {
+    Log.d("BRANCH"," : BRANCH 01");
+  }
 
   @Override protected void onResume() {
     super.onResume();
-    //disposable = db.createQuery(ListsItem.TABLES, ListsItem.QUERY)
-    //    .mapToList(ListsItem.MAPPER)
-    //    .observeOn(AndroidSchedulers.mainThread());
   }
+
 }
