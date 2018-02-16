b06c78cd6cc5456591ae2b885124c63040d6c1ff
lenovo\lencare <rameshbeandroid@gmail.com>
2018-02-16 14:17:44 +0530

    sample mvp structure

--- app/src/main/java/com/ramesh/gitsample/data/ApiService.java
+++
@@ -1,8 +0,0 @@
-package com.ramesh.gitsample.data;
-
-/**
- * Created by lencare on 05-Dec-17.
- */
-
-public class ApiService {
-}
--- app/src/main/java/com/ramesh/gitsample/ui/login/LoginActivity.java
+++ app/src/main/java/com/ramesh/gitsample/ui/login/LoginActivity.java
@@ -1,21 +1,52 @@
 package com.ramesh.gitsample.ui.login;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+import butterknife.BindView;
+import butterknife.OnClick;
 import com.ramesh.gitsample.R;
+import com.ramesh.gitsample.base.BaseActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseActivity<LoginPresenter> implements LoginView {
+
+  @BindView(R.id.et_user_name) EditText etUserName;
+
+  @BindView(R.id.et_user_password) EditText etPassword;
+
+  @Override public LoginPresenter intPresenter() {
+    Log.d("LogTrack", " : LoginPresenter() ");
+    return new LoginPresenter(this);
+  }
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_login);
+    Log.d("LogTrack", " : onCreate() ");
+    bindLayout(R.layout.activity_login);
+
   }
-
-
 
   @Override protected void onResume() {
     super.onResume();
+    presenter.isLogin();
   }
 
+  @OnClick() public void btUserLogin() {
+    String userName = etUserName.getText().toString();
+    String userPassword = etPassword.getText().toString();
+    presenter.loginRequest(userName,userPassword);
+  }
+
+  @Override public void onSuccess(Object model) {
+
+  }
+
+  @Override public void onError(String s) {
+    Toast.makeText(this, "" + s, Toast.LENGTH_LONG).show();
+  }
+
+  @Override public void onNavigation() {
+    /*Moving to another activity*/
+  }
 }
